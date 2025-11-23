d3c791b3aa6e34c0c86087dc87cf4fb2efe9548f
towfiqislambd <towfiqislambd@gmail.com>
2025-11-23 17:59:42 +0600

    added projects

--- src/app/_Components/Projects.tsx
+++ src/app/_Components/Projects.tsx
@@ -3,11 +3,6 @@ import Image from "next/image";
 import projectImg from "@/Assets/p1-BZE1zHxX.png";
 import projectImg2 from "@/Assets/p2-D5ICYa09.png";
 import projectImg3 from "@/Assets/p3-7fCS-KJe.png";
-import { RiReactjsLine } from "react-icons/ri";
-import { RiTailwindCssFill } from "react-icons/ri";
-import { BiLogoTypescript } from "react-icons/bi";
-import { SiMongodb } from "react-icons/si";
-import { SiExpress } from "react-icons/si";
 import reactIcon from "@/Assets/react.png";
 import tailwindIcon from "@/Assets/tailwind.png";
 import typescriptIcon from "@/Assets/ts.png";
@@ -19,7 +14,6 @@ const Projects = () => {
     {
       thumbnail: projectImg,
       title: "Digital Wallet",
-      category: "Full Stack Project",
       description:
         "High-performance Full Stack digital wallet built with React/TypeScript, featuring secure operations",
       technologies: [
@@ -35,7 +29,6 @@ const Projects = () => {
     {
       thumbnail: projectImg2,
       title: "LivingNest",
-      category: "Full Stack Project",
       description:
         "A room rental platform featuring user authentication and separate dashboards for users, members, and admins.",
       technologies: [
@@ -51,7 +44,6 @@ const Projects = () => {
     {
       thumbnail: projectImg3,
       title: "CarePoint",
-      category: "Fullstack Website",
       description:
         "CarePoint is a scalable platform for volunteer management, built with React, TailwindCSS, Firebase, Express, and MongoDB!",
       technologies: [
@@ -66,7 +58,6 @@ const Projects = () => {
     },
     {
       title: "GameScope",
-      category: "Fullstack Website",
       description:
         "A platform for reviewing and rating games, where users can share their experiences and ratings. Easy to use and community-driven",
       technologies: [
@@ -81,7 +72,6 @@ const Projects = () => {
     },
     {
       title: "Discount Pro",
-      category: "React Website",
       description:
         "Discount PRO is a coupon app for discovering and using discounts at e-commerce stores in Bangladesh, with responsive design.",
       technologies: [
@@ -96,7 +86,6 @@ const Projects = () => {
     },
     {
       title: "Paddy",
-      category: "JavaScript Website",
       description:
         "Peddy is a user-friendly pet adoption web app built with HTML, Tailwind CSS, DaisyUI, and JavaScript, offering a modern interface for easy pet adoption.",
       technologies: [
@@ -156,12 +145,12 @@ const Projects = () => {
                   ))}
                 </div>
 
-                <div className="flex gap-3">
+                <div className="flex gap-4">
                   <a
                     href={project.github}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="flex-1 py-2 bg-gray-700 flex gap-2 items-center rounded-lg text-center justify-center text-sm"
+                    className="flex-1 py-2 bg-gray-700 flex gap-1.5 items-center rounded text-center justify-center text-sm"
                   >
                     <Github className="w-4 h-4" />
                     GitHub
@@ -171,7 +160,7 @@ const Projects = () => {
                     href={project.demo}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="flex-1 py-2  bg-blue-500 flex gap-2 items-center rounded-lg text-center justify-center text-sm"
+                    className="flex-1 py-2  bg-blue-500 flex gap-1.5 items-center rounded text-center justify-center text-sm"
                   >
                     <ExternalLink className="w-4 h-4" />
                     Live Demo
